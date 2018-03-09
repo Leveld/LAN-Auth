ddbc700ec9b0e3d1a0d15373a2603ae37535aa45
Jourdan Clark <JourdanClark@users.noreply.github.com>
2018-03-09 08:43:36 -0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || '3002';
 
 mongoose.Promise = global.Promise;
 if (IS_PRODUCTION)
-  mongoose.connect('mongodb://process.env.DBUSER:process.env.DBPASSWORD@ds147274.mlab.com:47274/heroku_nf63v9rv');
+  mongoose.connect(`mongodb://${process.env.DBUSER}:${process.env.DBPASSWORD}@ds147274.mlab.com:47274/heroku_nf63v9rv`);
 else
   mongoose.connect('mongodb://localhost:2000/capstone-auth');
 
